41d4766b3cc6036eba7fe227b6f44ea60b138cc1
Vitor Leonel <vitor.leonel@loggi.com>
2019-10-09 20:00:53 -0300

    first commit

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import css from '../styles/main.scss';
+
+const linkStyle = {
+  marginRight: 15
+};
+
+const Header = () => (
+  <div>
+    <Link href="/">
+      <a className={css.words} style={linkStyle}>Home</a>
+    </Link>
+    <Link href="/about">
+      <a style={linkStyle}>About</a>
+    </Link>
+  </div>
+);
+
+export default Header;
